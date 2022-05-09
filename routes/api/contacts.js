b132066aba8b5b65e455c1cd48bfa480f2be7319
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2022-05-09 23:25:40 +0300

    to check

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const { isValidObjectId } = require("mongoose");
 const { ctrlWrapper } = require('../../middlewares');
 const ctrl = require('../../controllers/contacts');
 const { schemas } = require('../../models/contact');
@@ -9,9 +8,17 @@ const { validation } = require('../../middlewares');
 router.get('/', ctrl.getAll);
 router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
 router.post('/', validation(schemas.add), ctrlWrapper(ctrl.addContacts));
-router.put('/:contactId', validation(schemas.add), ctrlWrapper(ctrl.updateContacts));
+router.put(
+  '/:contactId',
+  validation(schemas.add),
+  ctrlWrapper(ctrl.updateContacts)
+);
 router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
-router.patch('/:contactId/favorite',  ctrlWrapper(ctrl.updateStatus));
+router.patch(
+  '/:contactId/favorite',
+  validation(schemas.favoriteSchema),
+  ctrlWrapper(ctrl.updateStatus)
+);
 
 module.exports = router;
-// validation(schemas.updateStatus),
+
